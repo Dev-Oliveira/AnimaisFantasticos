78e12c61975dafe55fcbf53def83a709cf7b2f2e
Dev-Oliveira <hellom-mendes@hotmail.com>
2022-06-10 19:46:52 +0100

    Entendendo Algumas propriedades Js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-const imgs = document.querySelectorAll("img");
+/* const imgs = document.querySelectorAll("img");
 
 imgs.forEach(function (item, index, array) {
+  console.log(item); // o item atual no loop
+  console.log(index); // o número do index
+  console.log(array); // a Array completa
+});
+ */
+
+const imgs = document.querySelectorAll("img");
+
+imgs.forEach((item, index, array) => {
   console.log(item); // o item atual no loop
   console.log(index); // o número do index
   console.log(array); // a Array completa
