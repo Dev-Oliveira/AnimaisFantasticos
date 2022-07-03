b9382445844575a6fe46ce8f1fafd2b9c6bee3c7
Hellom Mendes <hellom-mendes@hotmail.com>
2022-07-03 06:15:10 +0100

    corrige animação e modifica classe para datas

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function initTabNav() {
-  const tabMenu = document.querySelectorAll('.js-tabmenu li');
-  const tabContent = document.querySelectorAll('.js-tabcontent section');
+  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
+  const tabContent = document.querySelectorAll('[data-tab="content"] section');
 
   if (tabMenu.length && tabContent.length) {
 
