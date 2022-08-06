7d73da7639886d91d7d06c75a98f30cdf0a7b54a
Hellom Mendes <hellom-mendes@hotmail.com>
2022-08-06 20:48:10 +0100

    dropdown pt1

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
 import initToolTip from './modules/tooltip.js';
-
+import initDropdownMenu from './modules/dropdown-menu.js';
 
 initScrollSuave();
 initAnimacaoScroll();
